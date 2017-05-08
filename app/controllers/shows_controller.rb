3656049f4b6e4691b1752819874529a077655394
Ewen Carr <ewencarr@hotmail.com>
2017-05-08 15:42:38 +0100

    create route added

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,8 +1,18 @@
 class ShowsController < ApplicationController
 
+  def show_params
+    params.require(:show).permit([:title, :series, :description, :image, :programmeID])
+  end
+
+
   def index
     shows = Show.all
     render json: shows
+  end
+
+  def create
+    animal = Show.create(show_params)
+    render json: animal
   end
 
 end
